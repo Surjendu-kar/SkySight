b0f861502d6bbbbf8fbdd803adc227d7835bcddc
Dev-Surjendu <rahulkar9988@gmail.com>
2023-09-11 13:50:48 +0530

    add .env

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -106,8 +106,6 @@ function Home() {
   const [windSpeed, setWindSpeed] = React.useState<string | null>(null);
   const [forecastDays, setForecastDays] = React.useState<number>(3);
 
-  // const { user } = useAuthContext();
-
   const key = import.meta.env.VITE_NASA_API_KEY;
   const cityApi = `https://api.openweathermap.org/geo/1.0/direct?q=${userVal}&limit=5&appid=${key}`;
   const API = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,precipitation,rain,cloudcover,windspeed_10m`;
